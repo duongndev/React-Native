e07589852fe719cbb2310925643a2e39f65b7acf
Ng_Duong <77197933+ngduonng@users.noreply.github.com>
2023-05-29 12:53:30 +0700

    update code homeScreen.js

--- my-app/screens/homeScreen.js
+++ my-app/screens/homeScreen.js
@@ -1,16 +1,64 @@
-import { Button, View } from 'react-native';
-import React from 'react';
+import {Button, Image, SafeAreaView, ScrollView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import styles from "../styles/homeStyles";
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({navigation}) => {
+    const [students, setStudents] = useState([]);
+
+    async function getListStudent() {
+        try {
+            const API_URL = 'http://192.168.1.243:3000/students';
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            setStudents(data);
+            return data;
+        } catch (error) {
+            console.error('Fetch data failed ' + error);
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        getListStudent();
+    }, []);
+
     const navigateToLogin = () => {
         navigation.navigate('Login');
     };
 
     return (
-    <View>
-        <Button title='Go to Login Screen' onPress={navigateToLogin} />
-    </View>
-    ) 
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={{flex: 1, padding: 20}}>
+                <Button title='Go to Login Screen' onPress={navigateToLogin} />
+                <View>
+                    <Text style={styles.txtHeader}>List student</Text>
+                </View>
+                <View style={styles.container}>
+                    {students.map((item, index) => {
+                        return (
+                          <View style={styles.item} key={index}>
+                              <View style={styles.itemImageContainer}>
+                                  {item.gender === 'Male' ? (
+                                      <Image style={styles.itemImage} source={require('../assets/images/male.png')} resizeMode='contain' />
+                                  ) : (
+                                      <Image style={styles.itemImage} source={require('../assets/images/female.png')} resizeMode='contain' />
+                                  )}
+                              </View>
+                              <View style={{ paddingLeft: 15 }}>
+                                  <Text>{item.studentId}</Text>
+                                  <Text>{item.fullName}</Text>
+                                  <Text>{item.gender}</Text>
+                                  <Text>{item.email}</Text>
+                                  <Text>{item.dateOfBirth}</Text>
+                              </View>
+
+                          </View>
+                        );
+                    })}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 };
 
 export default HomeScreen;
---
+++ my-app/styles/homeStyles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  txtHeader: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  item: {
+    paddingVertical: 15,
+    borderBottomColor: '#E2E2E2',
+    borderBottomWidth: 0.5,
+    flexDirection: 'row'
+  },
+  itemImageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 100
+  },
+  itemImage: {
+    flex: 1,
+    width: undefined,
+    height: undefined
+  }
+});
+export default styles;
